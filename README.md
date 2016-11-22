b885d3ae6bfe089fd54f549144b2c44e27caaa9f
cunshuifengyun <wangshixiebo@126.com>
2016-11-22 15:37:41 +0800

    优化

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # crawer
 使用bluebird+rsuperagent+cheerio创建的拉取www.mzitu.com的爬虫
 
-本项目还在持续开发与优化中
+本项目还在持续开发与优化中,诸位若在使用中发现问题，请提交个issue，帮助作者改进。
--- bluebird.js
+++ bluebird.js
@@ -10,6 +10,9 @@ var _ = require('lodash');
 var imageRoot = './images';
 var ProgressBar = require('progress');
 
+var totalSuccessed = 0;
+var totalFailed = 0;
+var allPicNum = 0;
 
 start();
 function start() {
@@ -35,17 +38,38 @@ function start() {
 //superagent.get的promise化
 function getAsync(url) {
     return new Promise(function (resolve, reject) {
-        superagent.get(url)
-            .timeout(10000)
-            .end(function (err, res) {
-                if(err) {
-                    reject(err);
-                }else {
-                    resolve(res);
-                }
-            })
+
+        var retry = 0
+        getUrl(url);
+        function getUrl(url) {
+            superagent.get(url)
+                .timeout(10000)
+                .set({
+                    'User-Agent': "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:39.0) Gecko/20100101 Firefox/39.0"
+                })
+                .end(function (err, res) {
+                    retry++;
+
+                    if(err) {
+                        if(retry!==3) {
+                            console.log('这是第%j次尝试',retry);
+                            console.log(err.message);
+                            setTimeout(function () {
+                                getUrl(url);
+                            },10000)
+
+                        }else {
+                            reject(err);
+                        }
+                    }else {
+                        resolve(res);
+                    }
+                })
+        }
     })
 }
+
+
 //拉取主页，获取主页级页面信息
 function getPageInfo(url) {
     return getAsync(url)
@@ -65,7 +89,7 @@ function getImageGroupInfo(url,index) {
     console.log('开始获取第%j大页',index+1);
     return getAsync(url)
         .then(function(result) {
-            
+
             console.log('获取第%j大页成功',index+1);
             var $ = cheerio.load(result.text);
             var urls = [];
@@ -76,12 +100,12 @@ function getImageGroupInfo(url,index) {
             });
 
             return urls;
-            
+
         })
         .then(function(result) {
             result = _.flatten(result);
             return Promise.mapSeries(result, function(url, index) {
-                
+
                 return getImageShow(url,index);
             },{concurrency: 1})
         })
@@ -98,38 +122,38 @@ function getImageShow(url,index) {
     console.log('开始获取第%j组照片',index+1);
     return getAsync(url)
         .then(function(result) {
-            
-                console.log('获取第%j组照片成功',index+1);
-                var $ = cheerio.load(result.text);
-                var urls = [];
-                var picNum = $('.pagenavi a').last().prev().text();
-                var title = $('.main-title').text().replace(/[\/\\\|\<\>\*\:\?\"]/,'_');
-                var dirName = imageRoot+'/'+groupNum+'--'+title;
-                bar = new ProgressBar('downloading [:bar]:percent :etas',{
-                    complete: '=',
-                    incomplete: '',
-                    width: 20,
-                    total: Number(picNum)
-                });
-                groupNum++;
-                console.time('group');
-                mkdirp(dirName, function (err) {
-                    if(err) {
 
-                        console.log(err);
-                    }
+            console.log('获取第%j组照片成功',index+1);
+            var $ = cheerio.load(result.text);
+            var urls = [];
+            var picNum = $('.pagenavi a').last().prev().text();
+            var title = $('.main-title').text().replace(/[\/\\\|\<\>\*\:\?\"]/,'_');
+            var dirName = imageRoot+'/'+groupNum+'--'+title;
+            bar = new ProgressBar('downloading [:bar]:percent :etas',{
+                complete: '=',
+                incomplete: '',
+                width: 20,
+                total: Number(picNum)
+            });
+            groupNum++;
+            console.time('group');
+            mkdirp(dirName, function (err) {
+                if(err) {
 
-                });
-
-                for(var i=0; i<picNum; i++) {
-                    if(i==0) {
-                        urls.push(url);
-                    }else {
-                        urls.push(url+'/'+(i+1));
-                    }
+                    console.log(err);
                 }
-                return {targetDir:dirName, urls:urls}
-            
+
+            });
+
+            for(var i=0; i<picNum; i++) {
+                if(i==0) {
+                    urls.push(url);
+                }else {
+                    urls.push(url+'/'+(i+1));
+                }
+            }
+            return {targetDir:dirName, urls:urls}
+
         })
         .then(function(result) {
 
@@ -149,7 +173,12 @@ function getImageShow(url,index) {
                     failed++;
                 }
             }
+            allPicNum += result.length;
+            totalFailed += failed;
+            totalSuccessed += successed;
             console.log('一共有%j张图片需要下载，下载成功%j张,失败%j张',result.length,successed,failed);
+            console.log('累积有%j张图片需要下载，下载成功%j张,失败%j张',allPicNum,totalSuccessed,totalFailed);
+
             console.timeEnd('group');
         })
         .catch(function(err) {
@@ -157,15 +186,16 @@ function getImageShow(url,index) {
             return [];
         })
 }
+
 //拉取图片展示页面，获取图片src
 function getImageUrl(url, targetDir,index) {
     return getAsync(url)
         .then(function(result){
-            
+
             var $ = cheerio.load(result.text);
             var src = $('.main-image img').attr('src');
             return src;
-            
+
         })
         .then(function(result) {
 
